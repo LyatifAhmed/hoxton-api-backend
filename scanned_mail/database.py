8fe79a9e74f26600852201aa6208a5eee1cc9c84
LyatifAhmed <latifmutlu@gmail.com>
2025-04-13 20:47:42 +0100

    Log actual scanned_mail.db path to debug location

--- scanned_mail/database.py
+++ scanned_mail/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///scanned_mail.db"
+import os
+DATABASE_URL = f"sqlite:///{os.path.dirname(__file__)}/scanned_mail.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
